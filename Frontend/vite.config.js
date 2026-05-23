88c862ab47815d4adb86171c60e381f6e3b98352
Adam Kofroň <kofikofiadam@gmail.com>
2026-05-23 10:36:56 +0200

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://smart-guard-system.onrender.com/',
+        target: 'https://smart-guard-be.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
