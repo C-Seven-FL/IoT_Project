3d354cf98f2859d29b2eeff81d41ae9ef943e335
Adam Kofroň <kofikofiadam@gmail.com>
2026-05-23 09:50:28 +0200

    Update API proxy target to production URL

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3030',
+        target: 'https://smart-guard-system.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
